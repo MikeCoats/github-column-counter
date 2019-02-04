8265b610b6f010c82e9ef4d0cb32222b5e6afefb
Mike Coats <i.am@mikecoats.com>
2019-02-04 22:06:20 +0000

    Add a better get-columns query
    This now uses template strings to make passing in the parameters
    easier. It also has a naive deep-dive in to the returned object to
    pull out the values we want.

--- main.js
+++ main.js
@@ -8,6 +8,38 @@ const fetch = require('node-fetch')
 const cache = new boost.InMemoryCache()
 
 const token = process.env.TOKEN
+const owner = 'MikeCoats'
+const repo = 'github-column-counter'
+const project = 'Test Example Board'
+
+const repoProjectQuery = gql`
+{
+  repository(owner: "${owner}", name: "${repo}") {
+    projects(orderBy: { field: CREATED_AT, direction: DESC }, search:"${project}", first: 1) {
+      nodes {
+        columns(first:10) {
+          nodes {
+            name, cards(first:100) {
+              nodes {
+                content {
+                   ... on Issue {
+                    labels(first:10) {
+                      nodes {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
 const link = new boost.HttpLink({
   uri: 'https://api.github.com/graphql',
   fetch: fetch,
@@ -20,15 +52,17 @@ const client = new boost.ApolloClient({ cache, link })
 
 client
   .query({
-    query: gql`
-      {
-        repository(owner: "MikeCoats", name: "github-column-counter") {
-          projects(orderBy: { field: CREATED_AT, direction: DESC }) {
-            totalCount
-          }
-        }
-      }
-    `
+    query: repoProjectQuery
   })
-  .then(result => console.log(result))
+  .then(result => {
+    console.log(result.data)
+
+    const columns = result.data.repository.projects.nodes[0].columns.nodes.map(
+      x => {
+        return x.name
+      }
+    )
+
+    console.log(columns)
+  })
   .catch(err => console.error(err))
