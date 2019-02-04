3ada55a8e5ddc125d13e2ba831f001f5fea41d6f
Mike Coats <mike.coats@nature.scot>
2019-02-04 13:03:12 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+# GitHub Column Counter
+
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Description
+
+Tally up all the score tags on a GitHub project board.
+
+## Installation
+
+### Install it globally to use on many different repositories
+
+```sh
+npm install --global github-column-counter
+```
+
+### Install it locally to keep track on just this project
+
+```sh
+npm install --save-dev github-column-counter
+```
+
+## Usage
+
+```sh
+# TODO: write the usage documentation
+```
---
+++ main.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+/* jshint esversion: 6 */
+
+const boost = require('apollo-boost')
+const gql = require('graphql-tag')
+const fetch = require('node-fetch')
+
+const cache = new boost.InMemoryCache()
+const link = new boost.HttpLink({
+  uri: 'https://api.github.com/graphql',
+  fetch: fetch
+})
+const client = new boost.ApolloClient({ cache, link })
+
+client
+  .query({
+    query: gql`
+      {
+        repository(owner: "MikeCoats", name: "github-column-counter") {
+          projects(orderBy: { field: CREATED_AT, direction: DESC }) {
+            totalCount
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result))
+  .catch(err => console.error(err))
